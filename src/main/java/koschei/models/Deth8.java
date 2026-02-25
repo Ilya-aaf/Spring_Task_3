ced9bd4f60d9220df6beaadb76b6310af1d2ae9e
Тарасов Илья <ilyatarasow2005@gmail.com>
2026-02-25 16:34:26 +0300

    I created dependencies and linked beans in 3 ways: through a constructor, setters, and fields.

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    public Deth8() {
+    }
+
 }
